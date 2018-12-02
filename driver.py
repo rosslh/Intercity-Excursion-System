893fed9c850a5b6d4dcbdd103cf36456dca7ae14
B-Russell <43629901+B-Russell@users.noreply.github.com>
2018-12-02 18:32:17 -0500

    Update driver.py

--- driver.py
+++ driver.py
@@ -6,7 +6,15 @@ class Driver:
         self.weeklyScript();
     
     def mergeTransactionSummaryFiles(self):
-        pass
+        outfilename = "mergedTransactionSummaryFile.txt"
+        with open(outfilename, 'w') as outfile:
+            for filename in glob.glob("./transactionSummaryFiles/*"):
+                with open(filename, 'r') as readfile:
+                    for line in readfile:
+                        print(line)
+                        if "EOS" not in line:
+                            outfile.write(line)
+            outfile.write("EOS")
 
     def dailyScript(self):
         frontEnd(inputFile="./user-input/userInput1.txt", tsf="./transactionSummaryFiles/tsf1.txt")
